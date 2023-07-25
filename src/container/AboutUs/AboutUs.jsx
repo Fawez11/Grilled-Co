57820665e41854f8b0d91935f9d3ea70993906d4
Fawez11 <fawezfer12@gmail.com>
2023-07-25 15:57:47 +0200

    about as

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -13,7 +13,7 @@ const AboutUs = () => (
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <p className="p__opensans">At Grilled & Co, we take pride in offering a diverse and delectable range of dishes delivered right to your doorstep. Our passion for grilling and cooking shines through in every meal we prepare.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
 
@@ -24,7 +24,7 @@ const AboutUs = () => (
       <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
+        <p className="p__opensans">At Grilled & Co, we are proud to celebrate our rich history that spans over 50 years. Our journey began with a vision to create a culinary haven where food enthusiasts could indulge in the finest flavors and experience unparalleled dining.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
     </div>
